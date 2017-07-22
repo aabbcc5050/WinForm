e490a5407296513f9019a25ef73058680c07d095
aabbcc5050 <aabbcc5050@qq.com>
2017-07-23 02:01:05 +0800

    test

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using N1;
 
 namespace ConsoleApp2
 {
@@ -10,6 +11,19 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            A oa = new A();
+            oa.Myls();
+        }
+    }
+}
+
+namespace N1
+{
+    class A
+    {
+        public void Myls() {
+            Console.WriteLine("test");
+            Console.ReadLine();
         }
     }
 }
