e030721d87c5b92a704beb44fe36a064b5d483e1
aabbcc5050 <aabbcc5050@qq.com>
2017-07-25 07:40:05 +0800

    111

---
+++ C#Learn_Console/ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = 'a';
+            char b = '8';
+            char c = 'L';
+            char d = '.';
+            char e = '|';
+            char f = ' ';
+            Console.WriteLine("isLetter方法判断{1}是否字母:{0}",char.IsLetter(a),a);
+            Console.WriteLine("isDigit方法判断{1}是否为数字:{0}", char.IsDigit(b),b);
+            Console.WriteLine("isLetterOrDigit方法判断{1}是否为字母或者数字:{0}", char.IsLetterOrDigit(c),c);
+            Console.WriteLine("isLower方法判断{1}是否为小写字母:{0}", char.IsLower(a),a);
+            Console.WriteLine("isUpper方法判断{1}是否为大写字母:{0}", char.IsUpper(c),c);
+            Console.WriteLine("isPunctuation方法判断{1}是否为标点符号:{0}", char.IsPunctuation(d),d);
+            Console.WriteLine("isSeparator方法判断{1}是否为分隔符:{0}", char.IsSeparator(e),e);
+            Console.WriteLine("isWhiteSpace方法判断{1}是否为空白:{0}", char.IsWhiteSpace(f),f);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#Learn_Console/ConsoleApp14/ConsoleApp14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Str1 = "nihaoa";
+            string Str2 = "wobuhao";
+            string Str3 = null;
+            DateTime dt = new DateTime();
+            string strB = String.Format("{0:d}",dt);
+            Console.WriteLine("[{0}]equal[{1}]:[{2}]",Str1,Str2,String.Equals(Str1,Str2));
+            Console.WriteLine("[{0}]equal[{1}]:[{2}]", Str1, Str3, Str1.Equals(Str3));
+            Console.WriteLine("[{0}]equal[{1}]:[{2}]", Str3, Str1, Str1.Equals(Str3));//竟然没有空指针异常
+            Console.WriteLine(Str3);
+            Console.WriteLine(strB);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
