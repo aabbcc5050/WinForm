6f4a33cbad2a3d193564555095900e783993ea70
aabbcc5050 <aabbcc5050@qq.com>
2017-07-26 07:35:30 +0800

    111

---
+++ C#Learn_Console/ConsoleApp15/ConsoleApp15/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num = 1000;
+            StringBuilder sb = new StringBuilder("用一生下载你",100);
+            sb.Append("VS啦啦啦啦");
+            Console.WriteLine(sb);
+            sb.AppendFormat("{0:C}",Num);
+            Console.WriteLine(sb);
+            sb.Insert(0,"名称:");
+            Console.WriteLine(sb);
+            sb.Remove(15, sb.Length - 15);
+            Console.WriteLine(sb);
+            sb.Replace("名称:","一生所爱");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#Learn_Console/ConsoleApp16/ConsoleApp16/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            Hashtable hashtable = new Hashtable();
+            list.Add(1);
+            list.Add(2);
+            list.Add("nihao a ");
+            hashtable.Add("key1","value1");
+            hashtable.Add("key2","value2");
+            foreach (object obj in list) {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine(hashtable);
+            foreach (DictionaryEntry entry in hashtable) {
+                Console.WriteLine(entry.Key+":"+entry.Value);
+            }
+            Console.WriteLine("hashtable[\"keyname\"]:"+hashtable["key1"]);
+            hashtable.Clear();
+            foreach (DictionaryEntry entry in hashtable)
+            {
+                Console.WriteLine(entry.Key + ":" + entry.Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
