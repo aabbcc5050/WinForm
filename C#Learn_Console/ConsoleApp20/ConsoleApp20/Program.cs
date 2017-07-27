ad9055063c9ed8fdda7463144c0ededd89ab1f98
aabbcc5050 <aabbcc5050@qq.com>
2017-07-28 07:59:02 +0800

    111

---
+++ C#Learn_Console/ConsoleApp20/ConsoleApp20/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        class MyClass1
+        {
+            private int x = 0;
+            private int y = 0;
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int Add1() {
+                return X + Y;
+            }
+        }
+        class MyClass2 : MyClass1
+        {
+            private int z = 0;
+            public int Z { get; set; }
+            public int Add2() { return X + Y + Z; }
+        }
+        static void Main(string[] args)
+        {
+            MyClass1 myclass1 = new MyClass1();
+            MyClass2 myclass2 = new MyClass2();
+            myclass1.X = 3;
+            myclass1.Y = 5;
+            myclass2.X = 3;
+            myclass2.Y = 5;
+            myclass2.Z = 7;
+            Console.WriteLine(myclass1.Add1());
+            Console.WriteLine(myclass2.Add1());
+            Console.WriteLine(myclass2.Add2());
+        }
+    }
+}
---
+++ C#Learn_Console/ConsoleApp21/ConsoleApp21/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        class MyClass1 {
+            private int x = 0;
+            private int y = 0;
+            public int X { get; set; }
+            public int Y { get; set; }
+            public virtual int Add() {
+                return X + Y;
+            }
+        }
+        class MyClass2:MyClass1 //继承MyClass1
+        {
+            public override int Add() {
+                int x = 5;
+                int y = 7;
+                return x + y;
+            }
+        }
+        static void Main(string[] args)
+        {
+            MyClass2 myclass2 = new MyClass2();
+            MyClass1 myclass1 = (MyClass1)myclass2;
+            myclass1.X = 3;
+            myclass1.Y = 5;
+            Console.WriteLine(myclass2.Add());
+            Console.WriteLine(myclass1.Add());
+            Console.ReadLine();
+        }
+    }
+}
