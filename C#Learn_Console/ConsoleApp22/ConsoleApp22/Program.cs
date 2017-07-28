8132d346c930d7877630192d3b975abb00a36586
aabbcc5050 <aabbcc5050@qq.com>
2017-07-29 07:56:46 +0800

    111

---
+++ C#Learn_Console/ConsoleApp22/ConsoleApp22/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ImyInterface myclass = new MyClass();
+            myclass.ID = "TM";
+            myclass.Name = "C#学习";
+            myclass.ShowInfo();
+            Console.ReadLine();
+        }
+
+        interface ImyInterface {
+            string ID { get; set; }
+            string Name { get; set; }
+            void ShowInfo(); 
+        }
+
+        class MyClass : ImyInterface
+        {
+            string id = "";
+            string name = "";
+            public string ID { get =>  id;  set => id = value; }
+            public string Name { get => name; set => name = value; }
+
+            public void ShowInfo()
+            {
+                Console.WriteLine("编号\t姓名");
+                Console.WriteLine(ID+"\t"+Name);
+            }
+        }
+    }
+}
