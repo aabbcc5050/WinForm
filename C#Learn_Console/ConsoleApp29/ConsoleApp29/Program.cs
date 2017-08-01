c413668ea22b7a50d012e1683371dcae79e3379c
aabbcc5050 <aabbcc5050@qq.com>
2017-08-02 07:32:25 +0800

    111

---
+++ C#Learn_Console/ConsoleApp29/ConsoleApp29/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+    class Program
+    {
+        class test
+        {
+            public int MyInt(string a, string b) {
+                int int1;
+                int int2;
+                int num;
+                try {
+                    int1 = int.Parse(a);
+                    int2 = int.Parse(b);
+                    if (int2 == 0) {
+                        throw new DivideByZeroException();
+                    }
+                    num = int1 / int2;
+                    return num;
+                }
+                catch (DivideByZeroException ex) {
+                    Console.WriteLine("用零数除整数引发异常!");
+                    Console.WriteLine(ex.Message);
+                    return 0;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("请输入分子: ");
+                string str1 = Console.ReadLine();
+                Console.WriteLine("请输入分母: ");
+                string str2 = Console.ReadLine();
+                test tt = new test();
+                Console.WriteLine("分子除以分母的值: " + tt.MyInt(str1, str2));
+            }
+            catch (FormatException) {
+                Console.WriteLine("请输入数值格式的数据");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#Learn_Console/ConsoleApp30/ConsoleApp30/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "用一生下载你";
+            object obj = str;
+            try
+            {
+                int i = (int)obj;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally {
+                Console.WriteLine("程序执行完毕");
+            }
+            Console.ReadLine();
+        }
+    }
+}
