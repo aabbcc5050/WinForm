8654643ac999d360d0cb571496acfba2cffe3fd0
aabbcc5050 <aabbcc5050@qq.com>
2017-08-07 07:54:18 +0800

    mmm

---
+++ C#Learn_WinForm/WindowsFormsApp4/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.记载子窗体ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.水平平铺ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.垂直平铺ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.层叠排列ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.记载子窗体ToolStripMenuItem,
+            this.水平平铺ToolStripMenuItem,
+            this.垂直平铺ToolStripMenuItem,
+            this.层叠排列ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(696, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 记载子窗体ToolStripMenuItem
+            // 
+            this.记载子窗体ToolStripMenuItem.Name = "记载子窗体ToolStripMenuItem";
+            this.记载子窗体ToolStripMenuItem.Size = new System.Drawing.Size(96, 24);
+            this.记载子窗体ToolStripMenuItem.Text = "加载子窗体";
+            this.记载子窗体ToolStripMenuItem.Click += new System.EventHandler(this.记载子窗体ToolStripMenuItem_Click);
+            // 
+            // 水平平铺ToolStripMenuItem
+            // 
+            this.水平平铺ToolStripMenuItem.Name = "水平平铺ToolStripMenuItem";
+            this.水平平铺ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.水平平铺ToolStripMenuItem.Text = "水平平铺";
+            this.水平平铺ToolStripMenuItem.Click += new System.EventHandler(this.水平平铺ToolStripMenuItem_Click);
+            // 
+            // 垂直平铺ToolStripMenuItem
+            // 
+            this.垂直平铺ToolStripMenuItem.Name = "垂直平铺ToolStripMenuItem";
+            this.垂直平铺ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.垂直平铺ToolStripMenuItem.Text = "垂直平铺";
+            this.垂直平铺ToolStripMenuItem.Click += new System.EventHandler(this.垂直平铺ToolStripMenuItem_Click);
+            // 
+            // 层叠排列ToolStripMenuItem
+            // 
+            this.层叠排列ToolStripMenuItem.Name = "层叠排列ToolStripMenuItem";
+            this.层叠排列ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.层叠排列ToolStripMenuItem.Text = "层叠排列";
+            this.层叠排列ToolStripMenuItem.Click += new System.EventHandler(this.层叠排列ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 387);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 记载子窗体ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 水平平铺ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 垂直平铺ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 层叠排列ToolStripMenuItem;
+    }
+}
+
---
+++ C#Learn_WinForm/WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 记载子窗体ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            Form3 f3 = new Form3();
+            Form4 f4 = new Form4();
+            Form5 f5 = new Form5();
+            f2.MdiParent = this;
+            f3.MdiParent = this;
+            f4.MdiParent = this;
+            f5.MdiParent = this;
+            f2.Show();
+            f3.Show();
+            f4.Show();
+            f5.Show();
+        }
+
+        private void 水平平铺ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void 垂直平铺ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void 层叠排列ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.Cascade);
+        }
+    }
+}
