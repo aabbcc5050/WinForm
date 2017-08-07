26b11a918c84791aa0709e78716c07311531d90d
aabbcc5050 <aabbcc5050@qq.com>
2017-08-08 07:20:00 +0800

    00

---
+++ C#Learn_WinForm/WindowsFormsApp6/WindowsFormsApp6/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //编程方式向窗体添加控件
+            TextBox myTest = new TextBox();
+            myTest.Location = new Point(25,25);
+            this.Controls.Add(myTest);
+        }
+    }
+}
---
+++ C#Learn_WinForm/WindowsFormsApp9/WindowsFormsApp9/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Multiline = true;
+            textBox1.Text = "1234567890123456789";
+            textBox1.Height = 100;
+            textBox1.SelectionStart = 5;//选中状态开始
+            textBox1.SelectionLength = 5;//选择字符长度
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text;
+        }
+    }
+}
