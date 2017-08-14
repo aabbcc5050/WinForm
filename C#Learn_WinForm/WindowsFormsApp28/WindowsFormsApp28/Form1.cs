70e3d98fffd563af1b48284153349c2eb0dc078e
aabbcc5050 <aabbcc5050@qq.com>
2017-08-14 08:00:23 +0800

    000

---
+++ C#Learn_WinForm/WindowsFormsApp28/WindowsFormsApp28/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp28
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("项目不能为空");
+            }
+            else {
+                listView1.Items.Add(textBox1.Text.Trim());
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listView1.Items.Count == 0)
+            {
+                MessageBox.Show("项目中已经没有项目");
+            }
+            else {
+                listView1.Items.Clear();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("请选择要删除的项");
+            }
+            else {
+                listView1.Items.RemoveAt(listView1.SelectedItems[0].Index);
+                listView1.SelectedItems.Clear();
+            }
+        }
+    }
+}
---
+++ C#Learn_WinForm/WindowsFormsApp29/WindowsFormsApp29/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp29
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.Items.Add("001");
+            listView1.Items.Add("002");
+            listView1.Items.Add("003");
+            listView1.Items[2].Selected = true;
+        }
+    }
+}
