89356416d352063ff3ecdd172c3704509309eefe
aabbcc5050 <aabbcc5050@qq.com>
2017-08-29 07:23:26 +0800

    111

---
+++ C#GDI图形图像技术/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics ghs = this.CreateGraphics();
+            for (int i=1;i<6;i++) {
+                HatchStyle hs = (HatchStyle)(5 + i);
+                HatchBrush hb = new HatchBrush(hs,Color.White);
+                Rectangle rtl = new Rectangle(10, 50 * i, 50 * i, 50);
+                ghs.FillRectangle(hb,rtl);
+            }
+        }
+    }
+}
---
+++ C#GDI图形图像技术/WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Point p1 = new Point(100, 100);
+            Point p2 = new Point(150, 150);
+            LinearGradientBrush lgb = new LinearGradientBrush(p1, p2, Color.Black, Color.White);
+            Graphics ghs = this.CreateGraphics();
+            lgb.WrapMode = WrapMode.TileFlipX;
+            ghs.FillRectangle(lgb,15,15,150,150);
+        }
+    }
+}
