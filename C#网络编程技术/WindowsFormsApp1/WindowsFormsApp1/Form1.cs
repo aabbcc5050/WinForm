efe616fa05d84ede4a8bf7a8a3deb1df3b652207
aabbcc5050 <aabbcc5050@qq.com>
2017-09-03 22:17:58 +0800

    000

---
+++ C#网络编程技术/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == string.Empty)
+            {
+                MessageBox.Show("请输入主机地址!");
+            }
+            else {
+                textBox2.Text = string.Empty;
+                IPAddress[] ips = Dns.GetHostAddresses(textBox1.Text);
+                foreach (IPAddress ip in ips) {
+                    textBox2.Text = ip.ToString();
+                }
+                textBox3.Text = Dns.GetHostName();
+                textBox4.Text = Dns.GetHostByName(Dns.GetHostName()).HostName;
+            }
+        }
+    }
+}
---
+++ C#网络编程技术/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IPAddress[] ips = Dns.GetHostAddresses(textBox1.Text);
+            foreach (IPAddress ip in ips) {
+                label2.Text = "国际网络协议地址:" + ip.Address + "\nIP地址的地址族:" + ip.AddressFamily.ToString() +
+                    "\n是否IPv6链接本地地址:" + ip.IsIPv6LinkLocal;
+            }
+        }
+    }
+}
