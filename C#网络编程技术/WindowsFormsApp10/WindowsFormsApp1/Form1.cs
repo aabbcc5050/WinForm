2d7f187bee66c4fbaa4588d1dd9723c5d0851dfb
aabbcc5050 <aabbcc5050@qq.com>
2017-09-09 23:15:23 +0800

    000

---
+++ C#网络编程技术/WindowsFormsApp10/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1.Class1;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Class1 class1 = new Class1();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
