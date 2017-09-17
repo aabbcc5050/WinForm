d2385f0755d79334fcc3bd301037b8f5a851726b
aabbcc5050 <aabbcc5050@qq.com>
2017-09-17 21:39:04 +0800

    111

---
+++ C#注册表技术/重名文件/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private string documentKey = "{450D8FBA-AD25-11D0-98A8-0800361B1103}";
+        private string netKey = "{208D2C60-3AEA-1069-A2D7-08002B30309D}";
+        private string computerKey = "{20D04FE0-3AEA-1069-A2D8-08002B30309D}";
+        private string IeKey = "{871C5380-42A0-1069-A2EA-08002B30309D}";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i0 = checkBox1.Checked ? 0 : 1;
+            int i1 = checkBox2.Checked ? 0 : 1;
+            int i2 = checkBox3.Checked ? 0 : 1;
+            int i3 = checkBox4.Checked ? 0 : 1;
+            RegistryKey reg = null;
+            try
+            {
+                //部分没效果,可能是xp系统才有效(不管他)
+                reg = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel");
+                //reg = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel");
+                reg.SetValue(documentKey, i0, RegistryValueKind.DWord);
+                reg.SetValue(netKey, i1, RegistryValueKind.DWord);
+                reg.SetValue(computerKey, i2, RegistryValueKind.DWord);
+                reg.SetValue(IeKey, i3, RegistryValueKind.DWord);
+                reg.Close();
+                MessageBox.Show("执行完毕!请刷新页面或者重启!");
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ C#注册表技术/重名文件/WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string key1 = "NoRun";
+        string key2 = "NoSMHelp";
+        string key3 = "NoFind";
+        string key4 = "notraycontextmenu";
+        string key5 = "HideClock";
+
+        int hideValue = 1;
+        int showValue = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            regSetKeyAndValue(key1,hideValue);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            regSetKeyAndValue(key2, hideValue);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            regSetKeyAndValue(key3, hideValue);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            regSetKeyAndValue(key4, hideValue);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            regSetKeyAndValue(key5, hideValue);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            regSetKeyAndValue(key1, showValue);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            regSetKeyAndValue(key2, showValue);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            regSetKeyAndValue(key3, showValue);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            regSetKeyAndValue(key4, showValue);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            regSetKeyAndValue(key5, showValue);
+        }
+
+        private void regSetKeyAndValue(String key, int value)
+        {
+            try
+            {
+                RegistryKey reg = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\Explorer");
+                reg.SetValue(key, value);
+                reg.Close();
+                MessageBox.Show("执行成功!请刷新桌面或者重新启动计算机", "信息提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
