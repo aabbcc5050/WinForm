14b0f4bbfd283e3354aed10de2419bd78be42d77
aabbcc5050 <aabbcc5050@qq.com>
2017-09-20 07:53:14 +0800

    10

---
+++ C#项目实战/WindowsFormsApp1/WindowsFormsApp1/DataClass/MyBeans.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.DataClass
+{
+    class MyBeans
+    {
+
+        #region 全局变量
+        public static string Login_id = "";                                          //全局变量,记录当前登录的用户编号
+        public static string Login_Name = "";                                        //全局变量,记录当前登录的用户名
+        public static string Mean_SQL = "", Mean_Table = "", Mean_Field = "";        
+        public static SqlConnection My_con;
+        public static string M_str_sqlcon = "Data Source=mrlx\\lx;Database=db_PWMS;Userid=sa;PWD=";
+        public static int Login_n = 0;
+        public static string AllSql = "select * from tb_StaffBasic";
+        #endregion
+    }
+}
